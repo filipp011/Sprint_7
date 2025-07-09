bef651a5aa1dfd6b4e28ea464f61951fad14cfcf
Filipp Nis <f.nis61@gmail.com>
2025-07-09 08:03:58 +0300

    правка тестов

--- tests/test_create_curier.py
+++ tests/test_create_curier.py
@@ -4,11 +4,11 @@ import allure
 from data import API_CREATE_URL
 from utils import register_new_courier_and_return_login_password
 
-@allure.suite("Courier Registration Tests")
+@allure.suite("Тесты регистрации курьера")
 class TestCreateCourier:
 
-    @allure.title("Test Registration of a New Courier")
-    @allure.description("Verify that a new courier can be registered successfully.")
+    @allure.title("Тест регистрации нового курьера")
+    @allure.description("Проверка успешной регистрации нового курьера.")
     def test_registration_courier(self):
         url = API_CREATE_URL
         login_pass = register_new_courier_and_return_login_password()
@@ -25,8 +25,8 @@ class TestCreateCourier:
         # Проверка статуса ответа
         assert response.status_code == 201 and response.json() == {"ok": True}
 
-    @allure.title("Test Duplicate Courier Registration")
-    @allure.description("Verify that registering a courier with the same login, password, and name fails.")
+    @allure.title("Тест дублирования регистрации курьера")
+    @allure.description("Проверка, что регистрация курьера с теми же логином, паролем и именем завершается неудачей.")
     def test_duplicate_courier_registration(self):
         url = API_CREATE_URL
 
@@ -40,10 +40,10 @@ class TestCreateCourier:
         duplicate_response = requests.post(url, json=payload)
         response_json = duplicate_response.json()
         # Проверка статуса ответа на попытку создания дубликата
-        assert duplicate_response.status_code == 409 and response_json.get("message") == "Этот логин уже используется. Попробуйте другой." # Ожидаем статус 409 Conflict и сообщение
+        assert duplicate_response.status_code == 409 and response_json.get("message") == "Этот логин уже используется. Попробуйте другой."  # Ожидаем статус 409 Conflict и сообщение
 
-    @allure.title("Test Successful Courier Creation")
-    @allure.description("Verify that the response contains {'ok': true} after successful registration.")
+    @allure.title("Тест успешного создания курьера")
+    @allure.description("Проверка, что ответ содержит {'ok': true} после успешной регистрации.")
     def test_successful_courier_creation(self):
         url = API_CREATE_URL
         login_pass = register_new_courier_and_return_login_password()
@@ -62,8 +62,8 @@ class TestCreateCourier:
         ("", "valid_password", 400),  # Пустой логин
         ("valid_login", "", 400)       # Пустой пароль
     ])
-    @allure.title("Test Courier Creation with Empty Login or Password")
-    @allure.description("Verify that a 400 status code is returned when login or password is empty.")
+    @allure.title("Тест создания курьера с пустым логином или паролем")
+    @allure.description("Проверка, что возвращается код состояния 400, когда логин или пароль пустые.")
     def test_courier_creation_empty_login_or_password(self, login, password, expected_status):
         url = API_CREATE_URL
         
@@ -77,11 +77,11 @@ class TestCreateCourier:
         response = requests.post(url, json=payload)
         response_data = response.json()
 
-        # Проверяем, что статус-код ответа соответствует ожидаемому
+        # Проверяем, что статус-код ответа соответствует ожидаемому и сообщение
         assert response.status_code == expected_status and response_data["message"] == "Недостаточно данных для создания учетной записи"
 
-    @allure.title("Test Duplicate Courier Login Registration")
-    @allure.description("Verify that registering a courier with the same login fails.")
+    @allure.title("Тест дублирования регистрации курьера по логину")
+    @allure.description("Проверка, что регистрация курьера с тем же логином завершается неудачей.")
     def test_duplicate_courier_login_registration(self):
         url = API_CREATE_URL
 
@@ -93,6 +93,6 @@ class TestCreateCourier:
         }
         # Отправка POST-запроса для создания второго курьера с теми же данными
         duplicate_login = requests.post(url, json=payload)
-
-        # Проверка статуса ответа на попытку создания дубликата
-        assert duplicate_login.status_code == 409  # Ожидаем статус 409 Conflict
+        response_data = duplicate_login.json()
+        # Проверка статуса ответа на попытку создания дубликата по логину и сообщение
+        assert duplicate_login.status_code == 409 and response_data["message"] == "Этот логин уже используется. Попробуйте другой."
